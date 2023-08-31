9ce24f6984d2a7cf2c47add7db5000ffc2af5264
kairoski03 <kairoski03@gmail.com>
2023-08-31 18:47:01 +0900

    remove welldone code verification

--- apps/explorer/src/components/module/VerifyRegister.tsx
+++ apps/explorer/src/components/module/VerifyRegister.tsx
@@ -1,22 +1,14 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 import { Button, Text } from '@mysten/ui';
+import axios, { type AxiosResponse } from 'axios';
 import { type Dispatch, type SetStateAction, useState } from 'react';
 import FileUpload from 'react-material-file-upload';
 
-import { type ModuleType, PackageFile } from '~/components/module/PkgModulesWrapper';
+import { type ModuleType, type PackageFile } from '~/components/module/PkgModulesWrapper';
 import { useNetwork } from '~/context';
-import { useWdsBackend } from '~/hooks/useWdsBackend';
-import { Link } from '~/ui/Link';
+import { Input } from '~/ui/Input';
 
-export interface VerifyCheckResponse {
-	chainId: string;
-	packageId: string;
-	isVerified: boolean;
-	isRemix: boolean;
-	isSrcUploaded: boolean;
-	verifiedSrcUrl: string;
-}
 interface VerifyRegisterProps {
 	id?: string;
 	modules?: ModuleType[];
@@ -24,119 +16,97 @@ interface VerifyRegisterProps {
 	verified: boolean;
 	setVerified: Dispatch<SetStateAction<boolean>>;
 }
-interface VerificationResponse {
-	chainId: string;
+
+interface VerificationSourceUploadReqDto {
+	network: string;
 	packageId: string;
-	isVerified: boolean;
-	moduleResults: ModuleResult[];
-	isRemix?: boolean;
-	errMsg?: string;
-}
-interface ModuleResult {
-	packageId: string;
-	moduleName: string;
-	requestedTime: number;
-	isVerified: boolean;
-	onChainByteCode: string;
-	offChainByteCode: string;
+	srcZipFile: File;
 }
 
-const CHAIN_NAME = 'sui';
+interface VerificationSourceUploadResDto {
+	sourceFileId: string;
+}
+
+interface VerificationReqDto {
+	network: string;
+	packageId: string;
+	sourceFileId: string;
+}
+
+interface VerificationResDto {
+	isVerified: boolean;
+	errMsg?: string;
+}
 
 function VerifyRegister({ id, modules, verified, setVerified }: VerifyRegisterProps) {
 	const modulenames = modules?.map(([name]) => name);
-	// const [searchParams, setSearchParams] = useSearchParamsMerged();
-	// const [query, setQuery] = useState('');
 	const [query] = useState('');
 	const [version, setVersion] = useState<string>('v1.3.0');
-	const [isLoadingWithoutFile, setIsLoadingWithoutFile] = useState<boolean>(false);
 	const [isLoadingWithFile, setIsLoadingWithFile] = useState<boolean>(false);
-	const [errMsgWithoutFile, setErrMsgWithoutFile] = useState<string>('');
-	const [errMsgWithFile, setErrMsgWithFile] = useState<string>('');
+	const [errMsgWithFile, setErrMsgWithFile] = useState<string | undefined>('');
 	const [files, setFiles] = useState<File[]>([]);
+	const [verificationApiServer, setVerificationApiServer] = useState<string>(
+		'https://api.welldonestudio.io/compiler/sui',
+	);
 	const [network] = useNetwork();
-	const wdsBack = useWdsBackend();
-	const isExecuteDisabled = isLoadingWithoutFile || isLoadingWithFile || version === '';
+	const isExecuteDisabled = isLoadingWithFile || version === '' || files.length === 0;
 	if (!modulenames) {
 		return null;
 	}
 
-	console.log(`@@@ files`, files);
-
-	// const selectedModule =
-	// 	searchParams.get('module') && modulenames?.includes(searchParams.get('module')!)
-	// 		? searchParams.get('module')!
-	// 		: modulenames[0];
-	//
-
-	const welldoneCodeHref = 'https://docs.welldonestudio.io/code/getting-started';
-	const remixHref = 'https://remix.ethereum.org/';
-
-	const verifyWithoutFile = () => {
-		setIsLoadingWithoutFile(true);
-		wdsBack('GET', 'verification/sui/package-remix', null, {
-			chainId: network.toLowerCase(),
-			packageId: id,
-		})
-			.then((res) => {
-				const verificationRes = res as VerificationResponse;
-				console.log('remix verificationRes', verificationRes);
-				setVerified(verificationRes.isVerified);
-				setIsLoadingWithoutFile(false);
-
-				if (verificationRes.errMsg) {
-					setErrMsgWithoutFile(verificationRes.errMsg);
-				}
-			})
-			.catch((e) => {
-				console.log('@@@ error !!!');
-				console.error(e);
-				setIsLoadingWithoutFile(false);
-				setErrMsgWithoutFile(e.toString());
-			});
-	};
-	const verifyWithFile = () => {
+	const verifyWithFile = async () => {
+		if (!id) {
+			return;
+		}
 		setIsLoadingWithFile(true);
-		const curDate = new Date();
-		const body = {
-			chainName: CHAIN_NAME,
-			chainId: network.toLowerCase(),
-			compilerVersion: version,
-			packageId: id,
-			timestamp: curDate.getTime().toString(),
-			fileType: 'move',
-			zipFile: files[0],
-		};
-		wdsBack('POST', 's3Proxy/verification-src/sui', body)
-			.then(() => {
-				wdsBack('GET', 'verification/sui/package', null, {
-					chainId: network.toLowerCase(),
+		try {
+			const { status: sourcesResStatus, data: sourcesResData } = await axios.post<
+				VerificationSourceUploadResDto,
+				AxiosResponse<VerificationSourceUploadResDto>,
+				VerificationSourceUploadReqDto
+			>(
+				`${verificationApiServer}/verifications/sources`,
+				{
+					network: network.toLowerCase(),
 					packageId: id,
-					timestamp: curDate.getTime().toString(),
-				})
-					.then((res) => {
-						const verificationRes = res as VerificationResponse;
-						console.log('file verificationRes', verificationRes);
-						setVerified(verificationRes.isVerified);
-						setIsLoadingWithFile(false);
+					srcZipFile: files[0],
+				},
+				{
+					headers: {
+						'Content-Type': 'multipart/form-data',
+						Accept: 'application/json',
+					},
+				},
+			);
 
-						if (verificationRes.errMsg) {
-							setErrMsgWithFile(verificationRes.errMsg);
-							return;
-						}
-					})
-					.catch((e) => {
-						console.error(e);
-						setIsLoadingWithFile(false);
-						setErrMsgWithFile(e.toString());
-					});
-			})
-			.catch((e) => {
-				console.error(e);
-				setIsLoadingWithFile(false);
-				setErrMsgWithFile(e.toString());
+			if (sourcesResStatus !== 201) {
+				return;
+			}
+
+			const { status: verificationResStatus, data: verificationResData } = await axios.post<
+				VerificationResDto,
+				AxiosResponse<VerificationResDto>,
+				VerificationReqDto
+			>(`${verificationApiServer}/verifications`, {
+				network: network.toLowerCase(),
+				packageId: id,
+				sourceFileId: sourcesResData.sourceFileId,
 			});
+
+			if (verificationResStatus !== 201) {
+				return;
+			}
+
+			setVerified(verificationResData.isVerified);
+			setErrMsgWithFile(verificationResData.errMsg);
+		} catch (e: any) {
+			console.error(e);
+			setErrMsgWithFile(e.toString());
+		} finally {
+			setIsLoadingWithFile(false);
+		}
 	};
+
 	const filteredModules =
 		query === ''
 			? modulenames
@@ -149,75 +119,13 @@ function VerifyRegister({ id, modules, verified, setVerified }: VerifyRegisterPr
 	const onFileChange = (files: File[]) => {
 		setFiles(files);
 	};
-	// const submitSearch = useCallback(() => {
-	// 	if (filteredModules?.length === 1) {
-	// 		setSearchParams({
-	// 			module: filteredModules[0],
-	// 		});
-	// 	}
-	// }, [filteredModules, setSearchParams]);
-	// const onChangeModule = (newModule: string) => {
-	// 	setSearchParams({
-	// 		module: newModule,
-	// 	});
-	// };
+
+	const onVerificationApiServerChange = (e: any) => {
+		setVerificationApiServer(e.target.value);
+	};
+
 	return (
 		<div className="flex flex-col gap-5 border-b border-gray-45 md:flex-row md:flex-nowrap">
-			{/*<div className="w-full md:w-1/5">*/}
-			{/*<Combobox value={selectedModule} onChange={onChangeModule}>*/}
-			{/*	<div className="mt-2.5 flex w-full justify-between rounded-md border border-gray-50 py-1 pl-3 placeholder-gray-65 shadow-sm">*/}
-			{/*		<Combobox.Input*/}
-			{/*			onChange={(event) => setQuery(event.target.value)}*/}
-			{/*			displayValue={() => query}*/}
-			{/*			placeholder="Search"*/}
-			{/*			className="w-full border-none"*/}
-			{/*		/>*/}
-			{/*		<button onClick={submitSearch} className="border-none bg-inherit pr-2" type="submit">*/}
-			{/*			<Search24 className="h-4.5 w-4.5 cursor-pointer fill-steel align-middle text-gray-60" />*/}
-			{/*		</button>*/}
-			{/*	</div>*/}
-			{/*	<Combobox.Options className="absolute left-0 z-10 flex h-fit max-h-verticalListLong w-full flex-col gap-1 overflow-auto rounded-md bg-white px-2 pb-5 pt-3 shadow-moduleOption md:left-auto md:w-1/6">*/}
-			{/*		{filteredModules.length > 0 ? (*/}
-			{/*			<div className="ml-1.5 pb-2 text-caption font-semibold uppercase text-gray-75">*/}
-			{/*				{filteredModules.length}*/}
-			{/*				{filteredModules.length === 1 ? ' Result' : ' Results'}*/}
-			{/*			</div>*/}
-			{/*		) : (*/}
-			{/*			<div className="px-3.5 pt-2 text-center text-body italic text-gray-70">*/}
-			{/*				No results*/}
-			{/*			</div>*/}
-			{/*		)}*/}
-			{/*		{filteredModules.map((name) => (*/}
-			{/*			<Combobox.Option key={name} value={name} className="list-none md:min-w-fit">*/}
-			{/*				{({ active }) => (*/}
-			{/*					<button*/}
-			{/*						type="button"*/}
-			{/*						className={clsx(*/}
-			{/*							'mt-0.5 block w-full cursor-pointer rounded-md border px-1.5 py-2 text-left text-body',*/}
-			{/*							active*/}
-			{/*								? 'border-transparent bg-sui/10 text-gray-80'*/}
-			{/*								: 'border-transparent bg-white font-medium text-gray-80',*/}
-			{/*						)}*/}
-			{/*					>*/}
-			{/*						{name}*/}
-			{/*					</button>*/}
-			{/*				)}*/}
-			{/*			</Combobox.Option>*/}
-			{/*		))}*/}
-			{/*	</Combobox.Options>*/}
-			{/*</Combobox>*/}
-			{/*<div className="h-verticalListShort overflow-auto pt-3 md:h-verticalListLong">*/}
-			{/*	<VerticalList>*/}
-			{/*		{modulenames.map((name) => (*/}
-			{/*			<div key={name} className="mx-0.5 mt-0.5 md:min-w-fit">*/}
-			{/*				<ListItem active={selectedModule === name} onClick={() => onChangeModule(name)}>*/}
-			{/*					{name}*/}
-			{/*				</ListItem>*/}
-			{/*			</div>*/}
-			{/*		))}*/}
-			{/*	</VerticalList>*/}
-			{/*</div>*/}
-			{/*</div>*/}
 			<div className="h-full grow overflow-auto border-gray-45 pt-5 md:pl-7">
 				{verified ? (
 					<div className="text-subtitleMedium mb-4 mt-1 break-words">
@@ -239,6 +147,8 @@ function VerifyRegister({ id, modules, verified, setVerified }: VerifyRegisterPr
 										Please select Compiler Version :
 									</Text>
 
+									<div data-testid="inputs-card" />
+
 									<select
 										className="form-select rounded-md border border-gray-45 px-3 py-2 pr-8 text-bodySmall font-medium leading-[1.2] text-steel-dark shadow-button"
 										value={version}
@@ -256,45 +166,15 @@ function VerifyRegister({ id, modules, verified, setVerified }: VerifyRegisterPr
 										{/*<option value="v1.0.8">v1.0.8+commit.97d65f2</option>*/}
 									</select>
 								</div>
-								{/*<div className="mb-0.5 mt-3 break-words text-issue-dark">*/}
-								<div className="mb-2 mt-5">
-									<Text variant="body/medium" color="steel-dark">
-										If this code is deployed using{' '}
-										<Link
-											variant="text"
-											color="steel-darker"
-											href={welldoneCodeHref}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<span className="underline">WELLDONE Code</span>
-										</Link>{' '}
-										and{' '}
-										<Link
-											variant="text"
-											color="steel-darker"
-											href={remixHref}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<span className="underline">remix plugin ( CODE BY WELLDONE STUDIO )</span>
-										</Link>
-										, you can proceed verification without uploading a file.
-									</Text>
-								</div>
-								<Button
-									variant="primary"
-									size="md"
-									disabled={isExecuteDisabled}
-									loading={isLoadingWithoutFile}
-									onClick={verifyWithoutFile}
-								>
-									Verify without file
-								</Button>
-								<Text variant="pBodySmall/medium" color="issue">
-									{errMsgWithoutFile}
-								</Text>
 
+								<div style={{ marginTop: '1em' }}>
+									<Input
+										label="Verification API URL"
+										onChange={onVerificationApiServerChange}
+										value={verificationApiServer}
+										style={{ width: '40em' }}
+									/>
+								</div>
 								<div className="mb-1 mt-7">
 									<Text variant="body/medium" color="steel-dark">
 										Otherwise You can proceed verification with uploading a compressed file.
@@ -315,7 +195,7 @@ function VerifyRegister({ id, modules, verified, setVerified }: VerifyRegisterPr
 								<Button
 									variant="primary"
 									size="md"
-									disabled={isExecuteDisabled || files.length === 0}
+									disabled={isExecuteDisabled}
 									loading={isLoadingWithFile}
 									onClick={verifyWithFile}
 								>
--- apps/explorer/src/pages/object-result/ObjectResult.tsx
+++ apps/explorer/src/pages/object-result/ObjectResult.tsx
@@ -3,24 +3,34 @@
 
 import { useGetObject } from '@mysten/core';
 import { LoadingIndicator } from '@mysten/ui';
+import axios from 'axios';
+import { type AxiosResponse } from 'axios';
 import JSZip from 'jszip';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { translate, type DataType } from './ObjectResultType';
+import { type DataType, translate } from './ObjectResultType';
 import PkgView from './views/PkgView';
 import { TokenView } from './views/TokenView';
 import { PageLayout } from '~/components/Layout/PageLayout';
 import { ErrorBoundary } from '~/components/error-boundary/ErrorBoundary';
 import { type VersionInfo } from '~/components/module/DependencyView';
 import { type PackageFile } from '~/components/module/PkgModulesWrapper';
-import { type VerifyCheckResponse } from '~/components/module/VerifyRegister';
 import { useNetwork } from '~/context';
-import { useWdsBackend } from '~/hooks/useWdsBackend';
 import { Banner } from '~/ui/Banner';
 import { PageHeader } from '~/ui/PageHeader';
 
 const PACKAGE_TYPE_NAME = 'Move Package';
+
+interface VerificationCheckReqDto {
+	network: string;
+	packageId: string;
+}
+
+interface VerificationCheckResDto {
+	isVerified: boolean;
+	verifiedSrcUrl: string;
+}
 
 function Fail({ objID }: { objID: string | undefined }) {
 	return (
@@ -39,7 +49,6 @@ export function ObjectResult() {
 
 	const { id: objID } = useParams();
 	const { data, isLoading, isError, isFetched } = useGetObject(objID!);
-	const wdsBack = useWdsBackend();
 	const [packageFiles, setPackageFiles] = useState<PackageFile[]>([]);
 	const [versionInfo, setVersionInfo] = useState<VersionInfo>();
 	const [verified, setVerified] = useState<boolean>(false);
@@ -51,66 +60,87 @@ export function ObjectResult() {
 		}
 		const packageId = data.data?.objectId;
 
-		wdsBack('GET', 'verification/sui/verify-check', null, {
-			chainId: network.toLowerCase(),
-			packageId: packageId,
-		}).then((res) => {
-			const verifyCheckObj = res as VerifyCheckResponse;
-			console.log('verifyCheckObj', verifyCheckObj);
-			if (!verifyCheckObj.isVerified) {
+		async function verifyCheck() {
+			const { status, data: verificationCheckResult } = await axios.get<
+				VerificationCheckResDto,
+				AxiosResponse<VerificationCheckResDto>,
+				VerificationCheckReqDto
+			>(`https://api.welldonestudio.io/compiler/sui/verifications`, {
+				// todo
+				params: {
+					network: network.toLowerCase(),
+					packageId: packageId,
+				},
+			});
+			if (status !== 200) {
+				return;
+			}
+			console.log('verificationCheckResult', verificationCheckResult);
+			if (!verificationCheckResult.isVerified) {
 				setVerified(false);
 				setPackageFiles([]);
 				return;
 			}
-			fetch(verifyCheckObj.verifiedSrcUrl).then((resFile) => {
-				if (!resFile.ok) {
-					throw new Error('Network response was not ok');
-				}
 
-				resFile.arrayBuffer().then((arrayBuffer) => {
-					const blob = new Blob([arrayBuffer], { type: 'application/zip' });
-					const zip = new JSZip();
-					zip.loadAsync(blob).then((unzipped: JSZip) => {
-						const filePromises: Promise<PackageFile>[] = [];
-						unzipped.forEach((relativePath: string, file: JSZip.JSZipObject) => {
-							if (!file.dir) {
-								const filePromise = file.async('text').then(
-									(content: string): PackageFile => ({
-										relativePath: file.name,
-										content: content,
-									}),
-								);
-								filePromises.push(filePromise);
-							}
-						});
+			const { status: VerifiedSrcResStatus, data: blob } = await axios.get<Blob>(
+				verificationCheckResult.verifiedSrcUrl,
+				{
+					responseType: 'blob',
+				},
+			);
 
-						Promise.all(filePromises).then((packageFiles) => {
-							console.log('verified packageFiles', packageFiles);
-							setPackageFiles(
-								packageFiles.filter((packageFile) => {
-									if (
-										!(
-											packageFile.relativePath.includes('Move.toml') ||
-											packageFile.relativePath.includes('Move.lock')
-										)
-									) {
-										return packageFile;
-									}
-								}),
-							);
-							setVerified(verifyCheckObj.isVerified);
-						});
-					});
+			if (VerifiedSrcResStatus !== 200) {
+				throw new Error('Network response was not ok');
+			}
+
+			new JSZip().loadAsync(blob).then((unzipped: JSZip) => {
+				const filePromises: Promise<PackageFile>[] = [];
+				unzipped.forEach((relativePath: string, file: JSZip.JSZipObject) => {
+					if (!file.dir) {
+						const filePromise = file.async('text').then(
+							(content: string): PackageFile => ({
+								relativePath: file.name,
+								content: content,
+							}),
+						);
+						filePromises.push(filePromise);
+					}
+				});
+
+				Promise.all(filePromises).then((packageFiles) => {
+					console.log('verified packageFiles', packageFiles);
+					setPackageFiles(
+						packageFiles.filter(
+							(packageFile) =>
+								!(
+									packageFile.relativePath.includes('Move.toml') ||
+									packageFile.relativePath.includes('Move.lock')
+								),
+						),
+					);
+					setVerified(verificationCheckResult.isVerified);
 				});
 			});
-		});
+		}
 
-		wdsBack('GET', 'dependency-version-check/sui', null, {
-			network: network.toLowerCase(),
-			packageId: packageId,
-		}).then((res) => {
-			setVersionInfo(res as VersionInfo);
-		});
+		async function dependencyVersionCheck() {
+			const { status, data } = await axios.get<VersionInfo>(
+				`http://localhost:8000/verification/sui/dependency-version-check`,
+				{
+					params: {
+						network: network.toLowerCase(),
+						packageId: packageId,
+					},
+				},
+			);
+			if (status !== 200) {
+				return;
+			}
+			setVersionInfo(data);
+		}
+
+		verifyCheck().then();
+		dependencyVersionCheck().then();
 	}, [verified, data]);
 
 	if (isLoading) {
